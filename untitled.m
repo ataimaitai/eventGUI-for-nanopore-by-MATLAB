fc54efc3e25b18007f7ce3d64b1d9eaced472c15
ataimaitai <188677526@qq.com>
2025-07-23 14:42:45 +0800

    第五次修改，删除不必要的文件

--- untitled.m
+++
@@ -1,8 +0,0 @@
-close all;clear;clc;
-timeThreshold = [600,10000];
-deepthThreshold = [0,10000];
-eventTime = [100,300,400,600,800,100,1000,4000];
-eventDeepth=[10,-10,20000,10,200,10,11,12];
-pickTime = find(eventTime >= timeThreshold(1) & eventTime <= timeThreshold(2));
-pickDeepth = find(eventDeepth >= deepthThreshold(1) & eventDeepth <= deepthThreshold(2));
-commonIndices = pickTime(ismember(pickTime, pickDeepth));
